cedb3c62a3785a18c2b9d11dfec5a599ab44b267
Brian Chebon <brynnlamura@gmail.com>
2023-03-31 16:43:44 +0300

    added date section

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import Tweet from './Tweet'
 
 function App() {
   const [users, setUsers] = useState([
-    {name: "Ed", message:"hello there", likes: "500"},
-    {name: "John", message:"Iam the true king", likes: "800"},
-    {name: "Travesy", message:"My new channel is available", likes: "900"},
-    {name: "Mosh", message:"Welcome to my channel", likes: "400"},
-    {name: "Brian", message:"Iam a React Developer", likes: "1000"}
+    {name: "Ed", message:"hello there", likes: "500", date: "Aug 14th"},
+    {name: "John", message:"Iam the true king", likes: "800", date: "Aug 13th"},
+    {name: "Travesy", message:"My new channel is available", likes: "900", date: "Aug 14"},
+    {name: "Mosh", message:"Welcome to my channel", likes: "400", date: "Aug 12"},
+    {name: "Brian", message:"Iam a React Developer", likes: "1000", date: "Aug 15"}
   ]);
 
   return (
     <div className="App">
       {users.map(user =>(
-        <Tweet name={user.name} message={user.message} likes={user.likes}/>
+        <Tweet name={user.name} message={user.message} likes={user.likes} date={user.date}/>
       ))}
       
     </div>
--- src/Tweet.js
+++ src/Tweet.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-function Tweet({name, message, likes}) {
+function Tweet({name, message, likes,date}) {
     return(
         <div className="tweet">
             <h3>{name}</h3>
             <p>{message}</p>
             <h3>{likes}</h3>
+            <h3>{date}</h3>
         </div>
     )
     
